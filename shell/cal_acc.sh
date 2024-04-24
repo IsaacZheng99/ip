42a5e591d69db9aedb62321105985151f897068c
IsaacZheng99 <isaaczheng.cs@gmail.com>
2024-04-24 17:14:39 +0800

    add target_hltm_layer argument

--- shell/cal_acc.sh
+++ shell/cal_acc.sh
@@ -20,6 +20,7 @@ python cal_acc.py \
        --std "(0.229, 0.224, 0.225)" \
        --batch_size 8 \
        --target_model_layer "layer4" \
+       --target_hltm_layer 5 \
        --input_folder_path "./dataset/imagenet/data/val_sep" \
        --true_label 331 \
        --target_label 331 \
